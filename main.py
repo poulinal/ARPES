0c8c6abb272c078eb7bbd89bf3b3815f874331f3
poulin-al-northeastern <apoulinpff@gmail.com>
2025-09-26 18:07:14 -0400

    removed branch pushes from build.yml workflow; added tag pushes to build.yml workflow. Forced dark mode pallete in main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 import sys
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QPalette, QColor
+from PyQt6.QtCore import Qt
 
 import os
 
@@ -13,7 +15,25 @@ from src.ArdaHomePage import HOMEGUI
 # from src.arpesHome import ARPESHome
 
 if __name__ == "__main__":
+    palette = QPalette()
+    palette.setColor(QPalette.ColorRole.Window, QColor(53, 53, 53))
+    palette.setColor(QPalette.ColorRole.WindowText, Qt.GlobalColor.white)
+    palette.setColor(QPalette.ColorRole.Base, QColor(25, 25, 25))
+    palette.setColor(QPalette.ColorRole.AlternateBase, QColor(53, 53, 53))
+    palette.setColor(QPalette.ColorRole.ToolTipBase, Qt.GlobalColor.black)
+    palette.setColor(QPalette.ColorRole.ToolTipText, Qt.GlobalColor.white)
+    palette.setColor(QPalette.ColorRole.Text, Qt.GlobalColor.white)
+    palette.setColor(QPalette.ColorRole.Button, QColor(53, 53, 53))
+    palette.setColor(QPalette.ColorRole.ButtonText, Qt.GlobalColor.white)
+    palette.setColor(QPalette.ColorRole.BrightText, Qt.GlobalColor.red)
+    palette.setColor(QPalette.ColorRole.Link, QColor(42, 130, 218))
+    palette.setColor(QPalette.ColorRole.Highlight, QColor(42, 130, 218))
+    palette.setColor(QPalette.ColorRole.HighlightedText, Qt.GlobalColor.black)
+
     app = QApplication(sys.argv)
+    
+    app.setPalette(palette)
+    
     window = HOMEGUI(main_dir)
     window.show()
     
